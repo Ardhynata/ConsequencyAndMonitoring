09bdf505db5b8eee7a91a639d09aaa39f4ecf698
baruno <nonotsujatmiko@gmail.com>
2018-10-30 19:32:33 +0700

    pindah button tolak

--- app/src/main/java/com/depotlpgbanyuwangi/consequencyandmonitoringsystem/TambahChecker.java
+++ app/src/main/java/com/depotlpgbanyuwangi/consequencyandmonitoringsystem/TambahChecker.java
@@ -3,6 +3,7 @@ package com.depotlpgbanyuwangi.consequencyandmonitoringsystem;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -32,6 +33,7 @@ public class TambahChecker extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.tambah_checker);
 
         etUser = (EditText)findViewById(R.id.checkerID);
